d69a0c96e7fd7ab0698a2c31b9334b4b0b84879b
wnw <fkaus65@naver.com>
2022-08-16 14:45:41 +0900

    feat(renderTodo): add todo render Add todo array rendering function Closes #1

--- App.js
+++ App.js
@@ -3,9 +3,28 @@ let id = 0;
 
 const inputBoard = document.querySelector('.input-todo');
 
+// todo 렌더
+const renderTodo = () => {
+  const todoList = document.querySelector('.todos');
+  todoList.innerHTML = '';
+
+  todoArray.forEach(todo => {
+    const list = document.createElement('li');
+    list.classList.add('todo-item');
+
+    const todoElem = document.createElement('div');
+    todoElem.classList.add('todo');
+    todoElem.innerText = todo.content;
+
+    list.appendChild(todoElem);
+    todoList.appendChild(list);
+  });
+};
+
 // todo 추가
 const addTodo = (text, todoId) => {
   todoArray.push({ id: todoId, content: text, isCompleted: false });
+  renderTodo();
 };
 
 // 초기화면 설정
@@ -13,7 +32,7 @@ const init = () => {
   inputBoard.addEventListener('keyup', e => {
     if (e.code === 'Enter') {
       addTodo(e.target.value, id++);
-      inputBoard.value = '';
+      e.target.value = '';
     }
   });
 };
