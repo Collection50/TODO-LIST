db4790b0f47d52fdd2e7ab46010841a187e971b8
wnw <fkaus65@naver.com>
2022-08-16 16:30:24 +0900

    feat(checkTodo): add checkTodo function
    If todo is clicked, add CSS class 'checked'.
    
    We can complete or incomplete todo when we want
    
    Closes #2

--- App.js
+++ App.js
@@ -6,6 +6,25 @@ const initContent = (obj, prop) => {
   obj[prop] = '';
 };
 
+// 매개변수 id와 동일한 id를 갖고 있는 객체를 찾은 후
+// completed 값을 반전한다
+// 이후 바뀐 todoArray를 다시 렌더
+const checkTodo = id => {
+  todoArray.forEach(todo => {
+    if (todo.id === id) {
+      todo.isCompleted = !todo.isCompleted;
+    }
+  });
+  renderTodo();
+};
+
+// completed 값에 따른 css 클래스 추가 삭제
+const updateCompleted = (completed, todoElem) => {
+  if (completed) {
+    todoElem.classList.add('checked');
+  }
+};
+
 // todo 렌더
 const renderTodo = () => {
   const todoList = document.querySelector('.todos');
@@ -18,6 +37,8 @@ const renderTodo = () => {
     const todoElem = document.createElement('div');
     todoElem.classList.add('todo');
     todoElem.innerText = todo.content;
+    todoElem.addEventListener('click', () => checkTodo(todo.id));
+    updateCompleted(todo.isCompleted, todoElem);
 
     list.appendChild(todoElem);
     todoList.appendChild(list);
@@ -33,8 +54,8 @@ const addTodo = (text, todoId) => {
 // 초기화면 설정
 const init = () => {
   inputBoard.addEventListener('keyup', e => {
-    if (e.code === 'Enter') {
-      const { target } = e;
+    const { target } = e;
+    if (e.code === 'Enter' && target.value !== '') {
       addTodo(target.value, id++);
       initContent(target, 'value');
     }
